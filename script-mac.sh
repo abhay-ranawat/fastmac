85e0ce9de60cbbe1ffb1c283a3321a2bc8c6adba
Abhay <52895994+abhay-ranawat@users.noreply.github.com>
2020-11-07 22:21:04 +0530

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,1 +1,1 @@
-curl -fsSL https://code-server.dev/install.sh | sh && brew install agrinman/tap/tunnelto && code-server --auth none >/dev/null && tunnelto --port 8080
+curl -fsSL https://code-server.dev/install.sh | sh && brew install agrinman/tap/tunnelto && tunnelto --port 8080
