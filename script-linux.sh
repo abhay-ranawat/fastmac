fd30a09de6ba414138970968c2bab45e6b5fbc0a
Abhay <52895994+abhay-ranawat@users.noreply.github.com>
2020-11-07 22:38:37 +0530

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,1 +1,1 @@
-curl -fsSL https://code-server.dev/install.sh | sh && brew install agrinman/tap/tunnelto && code-server --auth none >/dev/null && tunnelto --port 8080
+curl -fsSL https://code-server.dev/install.sh | sh && cargo install tunnelto && code-server --auth none >/dev/null && tunnelto --port 8080
